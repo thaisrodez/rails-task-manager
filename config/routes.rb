0c9f30861c49e2ca40f8c4882cbd84965a196f55
thais de rodez <thaisderodez@gmail.com>
2021-08-10 12:22:59 +0200

    read all and show details

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list tasks
+  get 'tasks', to: 'tasks#index'
+  # read task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
